66be5567016df33ae8bd5a7b173159e9c67edbda
Suchitra Naik <susanaik@gmail.com>
2018-07-27 12:11:49 +0000

    user signup - route in controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < ApplicationController
+
+  get '/products' do
+    if logged_in?
+      @user = current_user
+      puts @user
+      erb :"/products/products"
+    else
+      # redirect to "/login"
+    end
+
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,32 @@
 class UsersController < ApplicationController
 
-    get 'signup' do
+    get '/signup' do
       erb :'/users/create_user'
     end
 
-    post 'signup' do
+    post '/signup' do
+      # raise params.inspect
+      if params[:username] == "" || params[:email] == "" || params[:password] == ""
+          redirect to '/signup'
+      else
+        @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+        @user.save
+        session[:user_id] = @user.id
+        redirect to :'/products'
+      end
+
     end
 
-    get 'login' do
-    end
+    # get 'login' do
+    # end
+    #
+    # post 'login' do
+    # end
+    #
+    # get 'logout' do
+    # end
 
-    post 'login' do
-    end
-
-    get 'logout' do
-    end
-
-    get 'users/:slug' do
-    end
+    # get 'users/:slug' do
+    # end
 
 end
