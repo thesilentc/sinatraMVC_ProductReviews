9472979e6038bef19781c8881c6a91020657f16d
Suchitra Naik <susanaik@gmail.com>
2018-07-29 17:40:46 +0000

    create edit and delete routes for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,7 +23,11 @@ class ProductsController < ApplicationController
   get '/products/:id' do
       if logged_in?
         @product = Product.find_by_id(params[:id])
-        erb :'/products/show_product'
+        if Product.exists?(@product)
+          erb :'/products/show_product'
+        else
+            redirect to "/login"
+        end
       else
         redirect to "/login"
       end
@@ -34,10 +38,44 @@ class ProductsController < ApplicationController
        redirect "/products/new"
      else
        @product = Product.create(:name => params["name"], :description => params["description"])
-       @product.user_id = current_user.id
        @product.save
        redirect to "/products/#{@product.id}"
      end
+   end
+
+
+   # update product
+   post '/products/:id' do
+   # update logic
+      # raise params.inspect
+     @product = Product.find_by_id(params[:id])
+
+     if params[:name] == "" || params[:description] == ""
+            redirect to "/products/#{@product.id}/edit"
+    else
+            # binding.pry
+            @product.update(:name => params[:name], :description => params[:description])
+            @product.save
+            redirect to "/products/#{@product.id}"
+     end
+   end
+
+   # edit link for product
+   get '/products/:id/edit' do
+     if logged_in?
+       @product = Product.find_by_id(params[:id])
+       erb :'/products/edit_product'
+    else
+      redirect to "/login"
+    end
+   end
+
+   # delete link for product
+   delete '/products/:id/delete' do
+     @product = Product.find_by_id(params[:id])
+     @product.delete
+     @product.save
+     redirect to '/products'
    end
 
 
