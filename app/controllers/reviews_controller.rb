69018f9b35f9c94bec324935866b9e9efe5e38e2
Suchitra Naik <susanaik@gmail.com>
2018-07-30 15:25:46 +0000

    update review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,6 +10,7 @@ class ReviewsController < ApplicationController
     end
   end
 
+  # create review
   post '/reviews/:id' do
     # binding.pry
     @product = Product.find_by_id(params[:id])
@@ -30,8 +31,9 @@ class ReviewsController < ApplicationController
   get '/reviews/:id' do
     if logged_in?
       @product = Product.find_by_id(params[:id])
+      @reviews = Review.where(:product_id => @product.id)
       @user = current_user
-      erb :"/reviews/reviews"
+      redirect to "/products/#{@product.id}"
     else
       redirect to "/login"
     end
@@ -52,6 +54,45 @@ class ReviewsController < ApplicationController
   #     end
   #   end
 
+  # update review
+  post '/reviews/:product_id/:review_id' do
+  # update logic
+    raise params.inspect
+    @product = Product.find_by_id(params[:product_id])
+    @review = Review.where(:review_id => params[:review_id])
+    @user = current_user
+    if params[:title] == "" || params[:comment] == ""
+           redirect to "/reviews/#{@product.id}/edit/#{@review.id}"
+   else
+           # binding.pry
+           @review = Review.update(:title => params["title"],
+                                  :review_comment => params["review_comment"],
+                                  :recommend => params["recommend"],
+                                  :rating => params["rating"],
+                                  :user_id => @user.id,
+                                  :product_id => @product.id)
+           @review.save
+           redirect to "/products/#{@product.id}"
+    end
+  end
 
+  # edit link for review
+  get '/reviews/:product_id/edit/:review_id' do
+    if logged_in?
+      @product = Product.find_by_id(params[:product_id])
+      @review = Review.find_by_id(params[:review_id])
+      erb :'/reviews/edit_review'
+   else
+     redirect to "/login"
+   end
+  end
+
+  # delete link for product
+  delete '/reviews/:id/delete' do
+    @review = Review.find_by_id(params[:id])
+    @review.delete
+    @review.save
+    redirect to '/reviews'
+  end
 
 end
