18bd3591ba20a6771842c57d0de2b0713a68ac0a
Suchitra Naik <susanaik@gmail.com>
2018-08-02 19:11:12 +0000

    remove slug from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,4 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :products, through: :reviews
 
-  def slug
-    username.downcase.gsub(" ", "-")
-  end
-
-  def self.find_by_slug(slug)
-    User.all.find do |user|
-      user.slug == slug
-    end
-  end
-
 end
